b5521a64d0fd6bd0ca3d9e753a420a6c7d8daa2e
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-01 16:51:53 -0600

    Create nfts.js

---
+++ src/utils/nfts.js
@@ -0,0 +1,20 @@
+module.exports = function(ChainstackApi) {
+  ChainstackApi.prototype.fetchNfts = async function({ chainName, walletAddress }) {
+    try {
+      const validatedToken = await this.validateToken();
+
+      const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${walletAddress}/nft_balances/`);
+
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Bearer ${validatedToken}`
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching NFTs for wallet:', walletAddress, 'on chain:', chainName, 'Error:', error);
+      throw new Error(`Failed to fetch NFTs for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
+    }
+  }
+}
