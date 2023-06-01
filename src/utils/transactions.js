21d2f6ae01c34fa32fd18b5da719ac13cb6bc122
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-01 16:45:32 -0600

    Create transactions.js

---
+++ src/utils/transactions.js
@@ -0,0 +1,46 @@
+module.exports = function(ChainstackApi) {
+  ChainstackApi.prototype.fetchRecentTransactions = async function({ chainName, walletAddress, currency, noLogs }) {
+    try {
+      const validatedToken = await this.validateToken();
+
+      const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${walletAddress}/transactions_v3/`);
+      const params = { 'quote-currency': currency, 'no-logs': noLogs };
+      url.search = new URLSearchParams(params).toString();
+
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Bearer ${validatedToken}`
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching transactions for wallet:', walletAddress, 'on chain:', chainName, 'Error:', error);
+      throw new Error(`Failed to fetch transactions for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
+    }
+  }
+
+  ChainstackApi.prototype.fetchContractDeploymentTransactions = async function({ chainName, walletAddress }) {
+    try {
+      const validatedToken = await this.validateToken();
+
+      const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${walletAddress}/transactions_v2/`);
+
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Bearer ${validatedToken}`
+        }
+      });
+
+      const transactions = response.data.data.items;
+
+      const contractDeploymentTransactions = transactions.filter(tx => tx.to_address === null);
+
+      return contractDeploymentTransactions;
+
+    } catch (error) {
+      console.error('Error fetching contract deployment transactions for wallet:', walletAddress, 'on chain:', chainName, 'Error:', error);
+      throw new Error(`Failed to fetch contract deployment transactions for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
+    }
+  }
+}
