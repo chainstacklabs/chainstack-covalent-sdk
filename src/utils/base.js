0c6a6de53cb83c6970d71907e4916e84c3d0bfd4
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-01 16:53:52 -0600

    Create base.js

---
+++ src/utils/base.js
@@ -0,0 +1,22 @@
+module.exports = function(ChainstackApi) {
+  ChainstackApi.prototype.fetchLogEvents = async function({ chainName, contractAddress, startingBlock, endingBlock }) {
+    try {
+      const validatedToken = await this.validateToken();
+
+      const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${contractAddress}/events_v2/`);
+      const params = { 'starting-block': startingBlock, 'ending-block': endingBlock };
+      url.search = new URLSearchParams(params).toString();
+
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Bearer ${validatedToken}`
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching log events for contract:', contractAddress, 'on chain:', chainName, 'Error:', error);
+      throw new Error(`Failed to fetch log events for contract ${contractAddress} on chain ${chainName}: ${error.message}`);
+    }
+  }
+}
