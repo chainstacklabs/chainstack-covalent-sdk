8bf5fd2b8b29b88f151ebd7629764d293f706578
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-04 16:39:27 -0600

    Update balances.js

--- src/utils/balances.js
+++ src/utils/balances.js
@@ -43,4 +43,55 @@ module.exports = function(ChainstackApi) {
       throw new Error(`Failed to fetch historical portfolio value for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
     }
   }
-}
+
+ChainstackApi.prototype.fetchERC20TokenTransfers = async function({ chainName, walletAddress, currency, contractAddress, startingBlock, endingBlock }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${walletAddress}/transfers_v2/`);
+    const params = { 
+      'quote-currency': currency, 
+      'contract-address': contractAddress, 
+      'starting-block': startingBlock, 
+      'ending-block': endingBlock 
+    };
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching ERC20 token transfers for wallet:', walletAddress, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch ERC20 token transfers for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
+  }
+ }
+
+ChainstackApi.prototype.fetchTokenHolders = async function({ chainName, tokenAddress, blockHeight, pageSize, pageNumber }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/tokens/${tokenAddress}/token_holders_v2/`);
+    const params = { 
+      'block-height': blockHeight, 
+      'page-size': pageSize,
+      'page-number': pageNumber 
+    };
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching token holders for token:', tokenAddress, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch token holders for token ${tokenAddress} on chain ${chainName}: ${error.message}`);
+  }
+ }
+} 
