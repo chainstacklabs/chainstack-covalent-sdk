2ac299f71c29b08d0fe6fce2bc6f6cffb8367f7e
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-04 18:18:37 -0600

    Update transactions.js

--- src/utils/transactions.js
+++ src/utils/transactions.js
@@ -46,4 +46,98 @@ module.exports = function(ChainstackApi) {
       throw new Error(`Failed to fetch contract deployment transactions for wallet ${walletAddress} on chain ${chainName}: ${error.message}`);
     }
   }
+
+ChainstackApi.prototype.getTransaction = async function({ chainName, txHash, quoteCurrency, noLogs, withDex, withNftSales, withLending }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/transaction_v2/${txHash}/`);
+    const params = {
+      'quote-currency': quoteCurrency,
+      'no-logs': noLogs,
+      'with-dex': withDex,
+      'with-nft-sales': withNftSales,
+      'with-lending': withLending
+    };
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching transaction with hash:', txHash, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch transaction with hash ${txHash} on chain ${chainName}: ${error.message}`);
+  }
 }
+
+ChainstackApi.prototype.getTransactionSummaryForAddress = async function({ chainName, walletAddress }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/address/${walletAddress}/transactions_summary/`);
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching transaction summary for address:', walletAddress, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch transaction summary for address ${walletAddress} on chain ${chainName}: ${error.message}`);
+  }
+}
+
+ChainstackApi.prototype.getAllTransactionsInBlock = async function({ chainName, blockHeight, quoteCurrency, noLogs }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/block/${blockHeight}/transactions_v3/`);
+    const params = { 
+      'quote-currency': quoteCurrency, 
+      'no-logs': noLogs 
+    };
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching all transactions in block:', blockHeight, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch all transactions in block ${blockHeight} on chain ${chainName}: ${error.message}`);
+  }
+}
+
+ChainstackApi.prototype.getBulkTimeBucketTransactionsForAddress = async function({ chainName, walletAddress, timeBucket, quoteCurrency, noLogs }) {
+  try {
+    const validatedToken = await this.validateToken();
+
+    const url = new URL(`${COVALENT_BASE_URL}/${chainName}/bulk/transactions/${walletAddress}/${timeBucket}/`);
+    const params = { 
+      'quote-currency': quoteCurrency, 
+      'no-logs': noLogs 
+    };
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${validatedToken}`
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching bulk time bucket transactions for address:', walletAddress, 'on chain:', chainName, 'Error:', error);
+    throw new Error(`Failed to fetch bulk time bucket transactions for address ${walletAddress} on chain ${chainName}: ${error.message}`);
+  }
+ }
+}  
