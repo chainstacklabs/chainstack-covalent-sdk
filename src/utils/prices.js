ed5a34dc0ab4aa795b69821623149709c8e18b4e
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-06-16 17:40:50 -0600

    Create prices.js

---
+++ src/utils/prices.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const { COVALENT_BASE_URL } = require('../config/config');
+
+module.exports = function(ChainstackApi) {
+  ChainstackApi.prototype.getHistoricalTokenPrices = async function({ chainName, quoteCurrency, contractAddress, from, to, pricesAtAsc=false }) {
+    try {
+      const validatedToken = await this.validateToken();
+
+      const url = new URL(`${COVALENT_BASE_URL}/pricing/historical_by_addresses_v2/${chainName}/${quoteCurrency}/${contractAddress}/`);
+      const params = { 
+        from: from, 
+        to: to, 
+        'prices-at-asc': pricesAtAsc 
+      };
+      url.search = new URLSearchParams(params).toString();
+
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Bearer ${validatedToken}`
+        }
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching historical token prices for contract address:', contractAddress, 'on chain:', chainName, 'Error:', error);
+      throw new Error(`Failed to fetch historical token prices for contract address ${contractAddress} on chain ${chainName}: ${error.message}`);
+    }
+  }
+}
